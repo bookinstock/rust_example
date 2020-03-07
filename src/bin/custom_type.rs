6e4b7070944f5d5b6fe959b2a9c4450449d6fbe3
Wende Lu <wendelu1991@gmail.com>
2020-03-07 16:30:27 +0800

    custome_type

---
+++ src/bin/custom_type.rs
@@ -0,0 +1,145 @@
+#[derive(Debug)]
+struct Person<'a> {
+    name: &'a str,
+    age: u8,
+}
+
+// 单元结构体
+struct Nil;
+
+// 元组结构体
+struct Pair(i32, f32);
+
+// 带有两个字段（field）的结构体
+#[derive(Debug)]
+struct Point {
+    x: f32,
+    y: f32,
+}
+
+// 结构体可以作为另一个结构体的字段
+#[allow(dead_code)]
+#[derive(Debug)]
+struct Rectangle {
+    p1: Point,
+    p2: Point,
+}
+
+fn main() {
+
+    println!("=================struct==============");
+    // 使用简单的写法初始化字段，并创建结构体
+    let name = "Peter";
+    let age = 27;
+    let peter = Person { name, age };
+    
+    // 以 Debug 方式打印结构体
+    println!("{:?}", peter);
+    
+    // 实例化结构体 `Point`
+    let point: Point = Point { x: 0.3, y: 0.4 };
+
+    // 访问 point 的字段
+    println!("point coordinates: ({}, {})", point.x, point.y);
+    
+    // 使用结构体更新语法创建新的 point，这样可以用到之前的 point 的字段
+    let new_point = Point { x: 0.1, ..point };
+    
+    // `new_point.y` 与 `point.y` 一样，因为这个字段就是从 `point` 中来的
+    println!("second point: ({}, {})", new_point.x, new_point.y);
+    
+    // 使用 `let` 绑定来解构 point
+    let Point { x: my_x, y: my_y } = point;
+
+    println!("x={}, y={}", point.x, point.y);
+
+    let rectangle = Rectangle {
+        // 结构体的实例化也是一个表达式
+        p1: Point { x: my_y, y: my_x },
+        p2: point,
+    };
+
+    println!("rectangle={:?}", rectangle);
+
+    // 实例化一个单元结构体
+    let _nil = Nil;
+
+    // 实例化一个元组结构体
+    let pair = Pair(1, 0.1);
+
+    // 访问元组结构体的字段
+    println!("pair contains {:?} and {:?}", pair.0, pair.1);
+
+    // 解构一个元组结构体
+    let Pair(integer, decimal) = pair;
+
+    println!("pair contains {:?} and {:?}", integer, decimal);
+
+    println!("=================enum==============");
+
+    let pressed = WebEvent::KeyPress('x');
+    // `to_owned()` 从一个字符串切片中创建一个具有所有权的 `String`。
+    let pasted  = WebEvent::Paste("my text".to_owned());
+    let click   = WebEvent::Click { x: 20, y: 80 };
+    let load    = WebEvent::PageLoad;
+    let unload  = WebEvent::PageUnload;
+
+    inspect(pressed);
+    inspect(pasted);
+    inspect(click);
+    inspect(load);
+    inspect(unload);
+}
+
+// 该属性用于隐藏对未使用代码的警告
+// 创建一个 `enum`（枚举）来对 web 事件分类。注意变量名和类型共同指定了 `enum`
+// 取值的种类：`PageLoad` 不等于 `PageUnload`，`KeyPress(char)` 不等于
+// `Paste(String)`。各个取值不同，互相独立。
+enum WebEvent {
+    // 一个 `enum` 可以是单元结构体（称为 `unit-like` 或 `unit`），
+    PageLoad,
+    PageUnload,
+    // 或者一个元组结构体，
+    KeyPress(char),
+    Paste(String),
+    // 或者一个普通的结构体。
+    Click { x: i64, y: i64 }
+}
+
+// 此函数将一个 `WebEvent` enum 作为参数，无返回值。
+fn inspect(event: WebEvent) {
+    match event {
+        WebEvent::PageLoad => println!("page loaded"),
+        WebEvent::PageUnload => println!("page unloaded"),
+        // 从 `enum` 里解构出 `c`。
+        WebEvent::KeyPress(c) => println!("pressed '{}'.", c),
+        WebEvent::Paste(s) => println!("pasted \"{}\".", s),
+        // 把 `Click` 解构给 `x` and `y`。
+        WebEvent::Click { x, y } => {
+            println!("clicked at x={}, y={}.", x, y);
+        },
+    }
+}
+
+
+/*
+
+# custom type
+
+- struct
+    - c-like struct
+    - tuple-like struct
+    - unit struct # -> trait
+    - deconstruct
+
+- enum
+
+- constant
+
+    - const
+
+    - static
+
+## ps
+
+*/

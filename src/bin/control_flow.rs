f36363f8907e02a78bab15992c82ac24bf4e1291
Wende Lu <wendelu1991@gmail.com>
2020-03-08 09:49:50 +0800

    fmt

--- src/bin/control_flow.rs
+++ src/bin/control_flow.rs
@@ -1,5 +1,4 @@
 fn main() {
-
     println!("=============if else===============");
     let n = 5;
 
@@ -11,23 +10,21 @@ fn main() {
         print!("{} is zero", n);
     }
 
-    let big_n =
-        if n < 10 && n > -10 {
-            println!(", and is a small number, increase ten-fold");
+    let big_n = if n < 10 && n > -10 {
+        println!(", and is a small number, increase ten-fold");
 
-            // 这个表达式返回一个 `i32` 类型。
-            10 * n
-        } else {
-            println!(", and is a big number, half the number");
+        // 这个表达式返回一个 `i32` 类型。
+        10 * n
+    } else {
+        println!(", and is a big number, half the number");
 
-            // 这个表达式也必须返回一个 `i32` 类型。
-            n / 2
-            // 试一试 ^ 试着加上一个分号来结束这条表达式。
-        };
+        // 这个表达式也必须返回一个 `i32` 类型。
+        n / 2
+        // 试一试 ^ 试着加上一个分号来结束这条表达式。
+    };
     //   ^ 不要忘记在这里加上一个分号！所有的 `let` 绑定都需要它。
 
     println!("{} -> {}", n, big_n);
-
 
     println!("=============for===============");
 
@@ -41,7 +38,7 @@ fn main() {
         if count == 3 {
             println!("three");
 
-            continue
+            continue;
         }
 
         println!("{:?}", count);
@@ -65,7 +62,6 @@ fn main() {
         }
 
         println!("never reach");
-
     }
 
     println!("exit outer loop");
@@ -83,7 +79,6 @@ fn main() {
     };
 
     println!("result={:?}", result);
-
 
     println!("===============while==============");
 
@@ -134,7 +129,7 @@ fn main() {
     for name in names.iter() {
         match name {
             &"wende" => println!("me!"),
-            name => println!("my friend {}!", name)
+            name => println!("my friend {}!", name),
         }
     }
 
@@ -143,7 +138,7 @@ fn main() {
     for name in names.into_iter() {
         match name {
             "wende" => println!("me!"),
-            name => println!("my friend {}!", name)
+            name => println!("my friend {}!", name),
         }
     }
 
@@ -156,12 +151,11 @@ fn main() {
         *name = match name {
             &mut "wende" => "lu",
             &mut "zhaobo" => "laobo",
-            _ => "kinda"
+            _ => "kinda",
         }
     }
 
     println!("names={:?}", names);
-
 
     println!("=============match============");
 
@@ -170,57 +164,52 @@ fn main() {
 
     match number {
         1 => println!("one"),
-        2  | 3 | 5 | 7 | 11 => println!("this is a prime!"),
+        2 | 3 | 5 | 7 | 11 => println!("this is a prime!"),
         13..=19 => println!("a teen"),
         _ => println!("ain't special"),
     };
 
-
     let boolean = true;
 
     let binary = match boolean {
-        false => 0, 
+        false => 0,
         true => 1,
     };
 
     println!("binary={:?}", binary);
 
-    match (0,1,2)  {
-        (a,b,c) => println!("a+b+c={:?}", a + b + c)
+    match (0, 1, 2) {
+        (a, b, c) => println!("a+b+c={:?}", a + b + c),
     }
 
     enum Color {
         Red,
         Blue,
         Green,
-        RGB(u32,u32,u32),
-        HSV(u32,u32,u32),
-        HSL(u32,u32,u32),
-        CMY(u32,u32,u32),
-        CMYK(u32,u32,u32,u32),
+        RGB(u32, u32, u32),
+        HSV(u32, u32, u32),
+        HSL(u32, u32, u32),
+        CMY(u32, u32, u32),
+        CMYK(u32, u32, u32, u32),
     }
 
-
-    let color = Color::RGB(122,18,32);
+    let color = Color::RGB(122, 18, 32);
 
     match color {
-        Color::Red   => println!("The color is Red!"),
-        Color::Blue  => println!("The color is Blue!"),
+        Color::Red => println!("The color is Red!"),
+        Color::Blue => println!("The color is Blue!"),
         Color::Green => println!("The color is Green!"),
-        Color::RGB(r, g, b) =>
-            println!("Red: {}, green: {}, and blue: {}!", r, g, b),
-        Color::HSV(h, s, v) =>
-            println!("Hue: {}, saturation: {}, value: {}!", h, s, v),
-        Color::HSL(h, s, l) =>
-            println!("Hue: {}, saturation: {}, lightness: {}!", h, s, l),
-        Color::CMY(c, m, y) =>
-            println!("Cyan: {}, magenta: {}, yellow: {}!", c, m, y),
-        Color::CMYK(c, m, y, k) =>
-            println!("Cyan: {}, magenta: {}, yellow: {}, key (black): {}!",
-                c, m, y, k),
+        Color::RGB(r, g, b) => println!("Red: {}, green: {}, and blue: {}!", r, g, b),
+        Color::HSV(h, s, v) => println!("Hue: {}, saturation: {}, value: {}!", h, s, v),
+        Color::HSL(h, s, l) => println!("Hue: {}, saturation: {}, lightness: {}!", h, s, l),
+        Color::CMY(c, m, y) => println!("Cyan: {}, magenta: {}, yellow: {}!", c, m, y),
+        Color::CMYK(c, m, y, k) => println!(
+            "Cyan: {}, magenta: {}, yellow: {}, key (black): {}!",
+            c, m, y, k
+        ),
     }
 
-    let  reference = &1;
+    let reference = &1;
 
     match reference {
         &val => println!("got a value via destructuring: {:?}", val),
@@ -253,39 +242,40 @@ fn main() {
     println!("mut_value={:?}", mut_value);
 
     match mut_value {
-         mut _m => {
+        mut _m => {
             _m += 10;
         }
     }
     println!("mut_value={:?}", mut_value);
 
-    
-    struct Foo {x:(i32, i32), y:i32}
+    struct Foo {
+        x: (i32, i32),
+        y: i32,
+    }
 
-    let foo = Foo {x :(1,2), y: 3};
-    let Foo {x:(a,b), y:c} = foo;
-    println!("a ={},b={},c={}", a,b,c);
+    let foo = Foo { x: (1, 2), y: 3 };
+    let Foo { x: (a, b), y: c } = foo;
+    println!("a ={},b={},c={}", a, b, c);
 
-    let Foo {y:a, x:b} = foo;
+    let Foo { y: a, x: b } = foo;
 
     println!("x={:?},y={:?}", a, b);
 
-    let Foo{x:a, ..} = foo;
+    let Foo { x: a, .. } = foo;
 
     println!("x={:?}", a);
 
+    let Foo { x, y } = foo;
 
-    let Foo{x,y} = foo;
+    println!("x={:?},y={:?}", x, y);
 
-    println!("x={:?},y={:?}",x,y );
-
-    let pair = (2,2);
+    let pair = (2, 2);
 
     let result = match pair {
-        (x,y) if x < y => "x less than y",
-        (x,y) if x > y => "x greater than y",
-        (x,y) if x == y => "x eq to y",
-        _ => "wtf"
+        (x, y) if x < y => "x less than y",
+        (x, y) if x > y => "x greater than y",
+        (x, y) if x == y => "x eq to y",
+        _ => "wtf",
     };
 
     println!("result={}", result);
@@ -296,8 +286,8 @@ fn main() {
 
     let result = match age() {
         0 => "I'm not born yet I guess".to_string(),
-        n @ 1 ..= 12 => format!("I'm a child of age {:?}", n),
-        n @ 13 ..= 19 => format!("I'm a teen of age {:?}", n),
+        n @ 1..=12 => format!("I'm a child of age {:?}", n),
+        n @ 13..=19 => format!("I'm a teen of age {:?}", n),
         n => format!("I'm an old person of age {:?}", n),
     };
 
@@ -318,7 +308,7 @@ fn main() {
     let x = Some(1);
     match x {
         Some(n) => println!("n={:?}", n),
-        None => println!("None")
+        None => println!("None"),
     }
 
     if let Some(n) = x {
@@ -334,15 +324,15 @@ fn main() {
     loop {
         match optional {
             Some(i) => {
-                if i>9 {
+                if i > 9 {
                     println!("greater than 9");
                     optional = None;
                 } else {
                     println!("i={:?}, try again", i);
-                    optional =  Some(i + 1);
+                    optional = Some(i + 1);
                 }
-            },
-            None => break
+            }
+            None => break,
         }
     }
     /////
@@ -359,8 +349,6 @@ fn main() {
         }
     }
 }
-
-
 
 /*
 
@@ -384,13 +372,13 @@ fn main() {
 
             - struct
 
-            - pointer 
+            - pointer
 
-        - guard 
-            - if 
+        - guard
+            - if
 
         - bind
-            - @ 
+            - @
 
 - loop, while, for
 

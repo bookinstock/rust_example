9b46791e0903f3dc46198173e334b8f9ff0dc94e
Wende Lu <wendelu1991@gmail.com>
2020-03-10 07:04:59 +0800

    lifetime

--- src/bin/scope.rs
+++ src/bin/scope.rs
@@ -376,27 +376,19 @@ fn test_borrow_ref() {
 }
 
 fn test_lifetime() {
-    // 下面使用连线来标注各个变量的创建和销毁，从而显示出生命周期。
-// `i` 的生命周期最长，因为它的作用域完全覆盖了 `borrow1` 和
-// `borrow2` 的。`borrow1` 和 `borrow2` 的周期没有关联，
-// 因为它们各不相交。
-    let i = 3; // Lifetime for `i` starts. ────────────────┐
-    //                                                     │
-    { //                                                   │
-        let borrow1 = &i; // `borrow1` lifetime starts. ──┐│
-        //                                                ││
-        println!("borrow1: {}", borrow1); //              ││
-    } // `borrow1 ends. ──────────────────────────────────┘│
-    //                                                     │
-    //                                                     │
-    { //                                                   │
-        let borrow2 = &i; // `borrow2` lifetime starts. ──┐│
-        //                                                ││
-        println!("borrow2: {}", borrow2); //              ││
-    } // `borrow2` ends. ─────────────────────────────────┘│
-    //                                                     │
-  // Lifetime ends. ─────────────────────────────────────┘
+    let mut x = 1;
 
+    {
+        let ref_x = &x;
+    }
+
+    {
+        let mut ref_x = &mut x;
+
+        *ref_x += 1;
+    }
+
+    println!("x={}", x);
 }
 
 fn test_lifetime_annotation() {

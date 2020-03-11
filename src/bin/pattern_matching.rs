426d3293fe72af5692293bd2756745df8716f783
Wende Lu <wendelu1991@gmail.com>
2020-03-11 23:22:00 +0800

    test match let if

--- src/bin/pattern_matching.rs
+++ src/bin/pattern_matching.rs
@@ -4,6 +4,12 @@
 
 # pattern matching
 
+- match
+
+- if-let (else)
+
+- while-let
+
 - literal
 
 - array
@@ -28,7 +34,53 @@
 
 
 fn main() {
+    test_match();
+    test_if_let();
+    test_while_let();
+    test_for();
+}
 
+fn test_match() {
+    let a = 1;
+
+    let x = match a {
+        0 => "zero",
+        1 => "one",
+        2 => "two",
+        _ => "???" 
+    };
+
+    println!("{}", x);
+}
+
+fn test_if_let()  {
+    let a = 1;
+
+    if let 1 = a {
+        println!("one!");
+    } else {
+        println!("???");
+    }
+}
+
+fn test_while_let() {
+    let mut stack = Vec::new();
+
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+
+    while let Some(top) = stack.pop() {
+        println!("{}", top);
+    }
+}
+
+fn test_for() {
+    let v = vec!['a', 'b', 'c'];
+
+    for (index, value) in v.iter().enumerate() {
+        println!("{} is at index {}", value, index);
+    }
 }
 
 fn test_literal() {

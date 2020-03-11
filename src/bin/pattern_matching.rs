3c3e6b8630f128f3dc463e111768dd9a9d22119f
Wende Lu <wendelu1991@gmail.com>
2020-03-11 22:36:58 +0800

    pattern matching

---
+++ src/bin/pattern_matching.rs
@@ -0,0 +1,64 @@
+
+
+/*
+
+# pattern matching
+
+- literal
+
+- array
+
+- enum
+
+- stuct
+
+- tuple
+
+- variable
+
+- wildcard
+
+- placeholder
+
+- refutable pattern
+
+- irrefutable pattern
+
+*/
+
+
+fn main() {
+
+}
+
+fn test_literal() {
+
+}
+
+fn test_array() {
+
+}
+
+fn test_enum() {
+
+}
+
+fn test_stuct() {
+
+}
+
+fn test_tuple() {
+
+}
+
+fn test_variable() {
+
+}
+
+fn test_wildcard() {
+
+}
+
+fn test_placeholder() {
+
+}
